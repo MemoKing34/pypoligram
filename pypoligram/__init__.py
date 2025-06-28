c8b9bc5a7c78e515f8362e5d2016fcff3ca4b9f6
MemoKing34 <karam24@itu.edu.tr>
2025-06-28 14:41:09 +0300

    Updated to version 0.1.2

--- pypoligram/__init__.py
+++ pypoligram/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2023-present, MemoKing34 <https://github.com/MemoKing34>"
 
